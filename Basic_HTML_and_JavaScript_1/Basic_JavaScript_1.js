62307a04d8dca58dd20c14337fc376132b740e07
austinmac95 <anthonymacpherson95@gmail.com>
2021-12-22 08:33:06 -0800

    Playing with JavaScript

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello, World!");// JavaScript Document
+
+function My_First_Function() {
+	var String = "Kiss me I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML =
+		result;
+}
